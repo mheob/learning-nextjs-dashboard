c3a04e7458cc0df7d9c734d239f645c2fb5d26b4
Alex Böhm <tools@boehm.work>
2023-11-02 13:35:05 +0100

    feat: after chapter 15 completed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
+import { signIn } from "@/auth";
+
 const InvoiceSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -113,5 +115,19 @@ export async function deleteInvoice(id: string) {
     return { message: "Deleted Invoice." };
   } catch (error) {
     return { message: "Database Error: Failed to Delete Invoice." };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialSignin";
+    }
+    throw error;
   }
 }
